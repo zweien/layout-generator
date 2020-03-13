267a099b69db2881709282ceccd63881a6a4044b
zweien <278954153@qq.com>
2020-03-13 11:27:53 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
+
 setup(
     name="layout-generator",
     version="0.1",
+    author='Zweien',
+    description='Layout dataset generator.',
     packages=find_packages(),
     install_requires=[
+        'tqdm'
         'scipy',
         'configargparse==1.0'
         ]
