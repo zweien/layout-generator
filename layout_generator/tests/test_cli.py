66e59707607c697ec40627655cb7004df815c857
zweien <278954153@qq.com>
2020-03-21 21:08:23 +0800

    update test_cli

--- layout_generator/tests/test_cli.py
+++ layout_generator/tests/test_cli.py
@@ -8,7 +8,7 @@ from layout_generator.utils import io
 def test_cli_default():
     # default config test
     options = parser.parse_args('')
-    assert options.nx == 20
+    assert options.length == 0.1
     assert options.prefix == 'Example'
     assert options.bcs == [ [[0.01, 0], [0.02, 0]], [[0.08, 0], [0.09, 0]] ]
     # cli test
