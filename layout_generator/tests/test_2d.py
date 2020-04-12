3e1abab84192ce05370b798a74bddecc3beea9d3
zweien <278954153@qq.com>
2020-04-13 01:30:53 +0800

    test: add test for `generator_plot`

--- layout_generator/tests/test_2d.py
+++ layout_generator/tests/test_2d.py
@@ -1,5 +1,4 @@
 import sys
-from pathlib import Path
 from layout_generator.utils import get_parser
 from layout_generator.utils import io
 from layout_generator.utils import visualize
@@ -9,13 +8,17 @@ from layout_generator.cli import main
 def test_2d_generator(tmp_path, capsys):
     sample_n = 10
     worker = 2
-    path = tmp_path / 'test'
+    path = tmp_path / "test"
     sys.argv = [
         "layout_generator",
-        "--data_dir", str(path),
-        "--bcs", "[]",
-        "--sample_n", str(sample_n),
-        "--worker", str(worker)
+        "--data_dir",
+        str(path),
+        "--bcs",
+        "[]",
+        "--sample_n",
+        str(sample_n),
+        "--worker",
+        str(worker),
     ]
     parser = get_parser()
     options, _ = parser.parse_known_args()
@@ -35,13 +38,19 @@ def test_2d_generator(tmp_path, capsys):
     assert u.shape == (options.nx,) * options.ndim
     assert u.min() >= options.u_D
 
-    # generator_plot
-    parser_plot = visualize.get_parser()
-    args_plot, _ = parser_plot.parse_known_args()
-    args_plot.path = options.data_dir
-    args_plot.dir = True
-    args_plot.worker = 2
-    visualize.plot_dir(
-        args_plot.path, out=args_plot.output, worker=args_plot.worker
-    )
-    assert len(list(Path(args_plot.path).glob("*.png"))) == options.sample_n
+    # plot dir
+    sys.argv = ["layout_plot", "-p", str(path), "--dir", "--worker", "2"]
+    visualize.main()
+    assert len(list(path.glob("*.png"))) == options.sample_n
+
+    # plot single file
+    file_path = next(path.glob("*.mat"))
+    sys.argv = [
+        "layout_plot",
+        "-p", str(file_path),
+        "-o", str(path / "o.png"),
+        "--worker", "2"
+    ]
+    visualize.main()
+    png_path = path / "o.png"
+    assert png_path.exists()
