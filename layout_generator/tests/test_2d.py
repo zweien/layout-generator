df9b5e780cdd09def78ba373a2f0683a33a1f74c
zweien <278954153@qq.com>
2020-04-13 02:02:47 +0800

    test: add bcs test for fenics_solver

--- layout_generator/tests/test_2d.py
+++ layout_generator/tests/test_2d.py
@@ -63,3 +63,45 @@ def test_2d_generator(tmp_path, capsys):
     visualize.main()
     png_path = path / "o.png"
     assert png_path.exists()
+
+
+def test_2d_bc(tmp_path):
+    try:
+        from pytest_cov.embed import cleanup_on_sigterm
+    except ImportError:
+        pass
+    else:
+        cleanup_on_sigterm()
+    sample_n = 10
+    worker = 4
+    path = tmp_path / "test"
+    sys.argv = [
+        "layout_generator",
+        "--data_dir",
+        str(path),
+        "--bcs",
+        "[[0.01, 0], [0.02, 0]]",
+        "--bcs",
+        "[[0.08, 0], [0.09, 0]]",
+        "--sample_n",
+        str(sample_n),
+        "--worker",
+        str(worker),
+    ]
+    parser = get_parser()
+    options, _ = parser.parse_known_args()
+
+    main()
+    data_dir = path
+    # assert data_dir == 's'
+    # data_path_list = os.listdir(options.data_dir)
+    data_path_list = list(data_dir.glob(f"*.{options.file_format}"))
+    assert data_dir.exists()
+    assert len(data_path_list) == options.sample_n
+    datum_path = data_path_list[0]
+
+    r = io.load_mat(datum_path)
+    assert set(["u", "F", "list", "xs", "ys"]).issubset(set(r.keys()))
+    u = r["u"]
+    assert u.shape == (options.nx,) * options.ndim
+    assert u.min() >= options.u_D
--- layout_generator/tests/test_3d.py
+++ layout_generator/tests/test_3d.py
@@ -32,3 +32,32 @@ def test_3d_generator(tmp_path):
     assert u.shape == (options.nx + 1,) * options.ndim
     assert F.max() == options.power[0]
     assert u.min() >= options.u_D
+
+
+def test_3d_bc(tmp_path):
+    config_path = Path(utils__file__).parent / "default3D.yml"
+    assert config_path.exists(), "default3D.yml does not exist"
+    parser = get_parser(config_path)
+    options, _ = parser.parse_known_args()
+    options.data_dir = tmp_path
+    options.bcs = [[[0, 0.05, 0.05], [0, 0.07, 0.07]]]
+    options.sample_n = 1
+    options.worker = 1
+    # assert options == 1
+    generate_from_cli(options)
+
+    data_dir = Path(options.data_dir)
+
+    data_path_list = list(data_dir.glob(f"*.{options.file_format}"))
+    assert data_dir.exists()
+    assert len(data_path_list) == options.sample_n
+
+    datum_path = data_path_list[0]
+
+    r = io.load_mat(datum_path)
+    assert set(["u", "F", "list", "xs", "ys", "zs"]).issubset(set(r.keys()))
+    F = r["F"]
+    u = r["u"]
+    assert u.shape == (options.nx + 1,) * options.ndim
+    assert F.max() == options.power[0]
+    assert u.min() >= options.u_D
